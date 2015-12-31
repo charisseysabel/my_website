dafde197e988417bb522d01ae139d9739cfbc60c
Charisse <charisse.ysabel930@gmail.com>
2015-12-31 23:13:36 +0100

    Add temporary images

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?= (file_get_contents("./templates/header.php"))?>
 
-    <!-- Main jumbotron for a primary marketing message or call to action -->
     <div class="jumbotron">
       <div class="container">
         <h1>Hi, I'm Charisse.</h1>
@@ -9,25 +8,17 @@
     </div>
 
     <div class="container">
-    	<h2>My work</h2>
-      <!-- Example row of columns -->
       <div class="row">
         <div class="col-md-4">
-          <h2>Heading</h2>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+          <img src="../img/map.jpg" alt="Kingdom of Thieves map" class="img_block"/>
         </div>
         <div class="col-md-4">
-          <h2>Heading</h2>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-       </div>
+          <img src="../img/map.jpg" alt="Kingdom of Thieves map" class="img_block"/>
+        </div>
         <div class="col-md-4">
-          <h2>Heading</h2>
-          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+          <img src="../img/map.jpg" alt="Kingdom of Thieves map" class="img_block"/>
         </div>
       </div> <!-- /row -->
-
+    </div><!-- /container -->
 
 <?= (file_get_contents("./templates/footer.php") )?>
