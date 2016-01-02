affbf96a03aaec279db8e501731187b11ae45aa8
Charisse <charisse.ysabel930@gmail.com>
2016-01-02 23:33:05 +0100

    Add content

--- templates/footer.php
+++ templates/footer.php
@@ -1,9 +1,8 @@
-      <footer>
-        <p>&copy; 2015 Company, Inc.</p>
-      </footer>
-    </div> <!-- /container -->
+	<footer>
+        <p>&copy; 2015 Charisse Ysabel De Torres</p>
+    </footer>
 
-	<!-- Bootstrap core JavaScript
+<!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
--- templates/header.php
+++ templates/header.php
@@ -1,18 +1,20 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
+  <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <meta name="description" content="">
     <meta name="author" content="">
 
-  	<link rel="stylesheet" href="../css/bootstrap.min.css"/>
-  	<link rel="stylesheet" href="../css/styles.css" />
+    <link rel="stylesheet" href="../css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="../css/styles.css" />
 
     <!-- fonts -->
-    <link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Quicksand' rel='stylesheet' type='text/css'>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
@@ -20,32 +22,34 @@
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
 
-	<title>Charisse Ysabel De Torres</title>
+  <title>Charisse Ysabel De Torres | Portfolio</title>
 </head>
 
   <body>
+    <div class="container">
 
-    <!-- Fixed navbar -->
-    <nav class="navbar navbar-default navbar-fixed-top">
-      <div class="container">
-
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <img src="../img/cyLogo.png" alt="Charisse's logo" width="50" height="50"/>
-        </div>
-
-        <div id="navbar" class="navbar-collapse collapse">
+    <!-- Static navbar -->
+      <nav class="navbar navbar-default">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+              <span class="sr-only">Navigate</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a href="../index.php"><img src="../img/cyLogo.png" alt="Charisse's logo" width="50" height="50"/></a>
+          </div>
+          
+          <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav navbar-right">
-              <li><a href="../index.php">Home</a></li>
-              <li><a href="https://github.com/charisseysabel">GitHub</a></li>
-              <li><a href="../about.php">About</a></li>
+              <li class="hvr-underline-reveal"><a href="../about.php">Portfolio</a></li>
+              <li class="hvr-underline-reveal"><a href="../about.php">About</a></li>
+              <li class="hvr-underline-reveal"><a href="#">Blog</a></li>
+              <li class="hvr-underline-reveal"><a href="../about.php">Contact</a></li>
             </ul>
-        </div><!--/.nav-collapse -->
-      
-      </div>
-    </nav>
+          </div><!--/.nav-collapse -->
+
+        </div><!--/.container-fluid -->
+      </nav>
+    </div>
