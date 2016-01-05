7504a1be7d0053c442b9ab35d82a9fb507da0854
Charisse <charisse.ysabel930@gmail.com>
2016-01-05 23:43:55 +0100

    Transfer contact from index to contact page

--- contact.php
+++ contact.php
@@ -7,13 +7,33 @@
 </div>
 
 <div class="container">
-  <div class="content">
-   <article>
-    <p>
-      Thank you for your interest. Kindly email me at charisse.ysabel930@gmail.com
-    </p>
-  </article>
-  </div>
-</div>
+      <section>
+        <p>Inquiries and / or greetings are all welcome!<br>
+          Please fill out the form and I will reply to you as soon as I can.</p>
+      </section>
+    </div>
+
+<div class="container send_note">
+      <div class="form_container">
+        <form method="post" action="../send_a_msg.php">
+          <p>Name<span class="asterisk">&#42;</span>
+            <input type="text" placeholder="eg.Charisse De Torres" name="name" required/>
+          </p>
+          
+          <p>Email<span class="asterisk">&#42;</span>
+            <input type="email" placeholder="eg. cydetorres@gmail.com" name="email" required/>
+          </p>
+
+          <p id="msg_label">Message<span class="asterisk">&#42;</span>
+            <textarea cols="60" rows="10" name="message" required></textarea>
+          </p>
+
+          <p id="submit_notice">You will receive a copy of your form for reference.
+            I promise to keep your details private.
+          </p>
+          <button name="submit">Submit</button>
+        </form>
+      </div>
+    </div>
 
 <?= (file_get_contents("./templates/footer.php") )?>
--- index.php
+++ index.php
@@ -1,93 +1,46 @@
-<?= (file_get_contents("./templates/header.php"))?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-    <div class="jumbotron">
+    <link rel="stylesheet" href="/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="/css/styles.css" />
+
+    <!-- fonts -->
+    <link href='https://fonts.googleapis.com/css?family=Quicksand' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Lora:400italic' rel='stylesheet' type='text/css'>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+  <title>Charisse Ysabel De Torres | Under Construction</title>
+</head>
+
+  <body>
+  <div class="jumbotron">
       <div class="container">
-        <h1>Hi, I'm Charisse</h1>
-        <p class="greetings">I like to design and program stuff.</p>
-      </div>
-    </div>
-
-    <div class="section">
-      <p>Works</p>
-    </div>
-
-    <div class="container">
-      <div class="col-md-4">
-        <a href="rocket.php">
-          <img src="../img/rocket-thumb.jpg" alt="rocket" class="workImg"/>
-          <p class="workDescription">
-            <span class="project_name">Rocket</span><br>
-            A cashflow web application built from the ground up
-          </p>
-        </a>
-      </div>
-
-      <div class="col-md-4">
-        <a href="#">
-          <img src="../img/map.jpg" alt="map" class="workImg"/>
-          <p class="workDescription">
-            <span class="project_name">Backpack</span><br>
-            A database of game items
-          </p>
-        </a>
-      </div>
-
-      <div class="col-md-4">
-        <a href="#">
-          <img src="../img/map.jpg" alt="map" class="workImg"/>
-          <p class="workDescription">
-            <span class="project_name">Coming soon!</span></br>
-            Future project
-          </p>
-        </a>
-      </div>
-    </div><!-- container collapse -->
-
-    <div class="section">
-      <p>A Brief Summary</p>
-    </div>
-
-    <div class="container">
-      <section>
-        <p id="brief_sum">I'm a junior developer with a background in graphic design.</p>
-        <p>
-          insert some random text here that would blow people's minds.insert some random text here that would blow people's minds.insert some random text here that would blow people's minds.insert some random text here that would blow people's minds.insert some random text here that would blow people's minds.insert some random text here that would blow people's minds.insert some random text here that would blow people's minds.insert some random text here that would blow people's minds.insert some random text here that would blow people's minds.<br>
-          insert some random text here that would blow people's minds.insert some random text here that would blow people's minds.insert some random text here that would blow people's minds.insert some random text here that would blow people's minds.insert some random text here that would blow people's minds.
+        <h1>Under Construction</h1>
+        <p class="greetings">Thanks for visiting. However, I'm still setting things up over here.<br>
+          Why don't you come back later?
         </p>
-      </section>
-    </div>
-
-    <div class="section">
-      <p>Leave a note</p>    
-    </div>
-
-    <div class="container">
-      <section>
-        <p>General inquiries and / or greetings are welcome! I love receiving kind emails.<br>Please fill out the form and I will reply to you as soon as I can.</p>
-        
-      </section>
-    </div>
-
-    <div class="container send_note">
-      <div class="form_container">
-        <form method="post" action="send_a_msg.php">
-          <p>Name<span class="asterisk">&#42;</span>
-            <input type="text" placeholder="eg.Charisse De Torres" name="name" required/>
-          </p>
-          
-          <p>Email<span class="asterisk">&#42;</span>
-            <input type="email" placeholder="eg. cydetorres@gmail.com" name="email" required/>
-          </p>
-
-          <p id="msg_label">Message<span class="asterisk">&#42;</span>
-            <textarea cols="60" rows="10" name="message" required></textarea>
-          </p>
-
-          <p id="submit_notice">I promise to keep your details private.</p>
-          <button name="">Submit</button>
-        </form>
       </div>
     </div>
 
+<!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="/scripts/bootstrap.min.js"></script>
 
-<?= (file_get_contents("./templates/footer.php") )?>
+</body>
+</html>
