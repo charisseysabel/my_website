1ea30dd4e3cecd9a9f8056d7c7f7e1e52799174f
Charisse <charisse.ysabel930@gmail.com>
2016-01-09 16:25:13 +0100

    Rewrite short bio

--- about.php
+++ about.php
@@ -3,23 +3,44 @@
 <div class="jumbotron work_title">
   <div class="container">
     <h1>About</h1>
-    <p>Philosophy &#8226; Skills &#8226; Interests</p>
+    <p>Hi, I'm Charisse.</p>
+    <p>I'm a junior developer with a graphic design background.<br>
+    Occassional writer, artist and forever lover of dessert.</p>
   </div>
 </div>
 
-<div class="container work_article">
-  <div class="col-xs-6 col-lg-4 article_caption">
-      <h1>"Be simple."</h1>
+<div class="container">
+  <img src="../img/charisse.jpg" id="me"/>
+
+  <div class="section">
+      <p>A really brief history</p>
   </div>
 
-  <div class="col-xs-12 col-sm-6 col-lg-8 content">
-    <img src="../img/charisse.jpg" alt="Charisse in front of the mirror" id="me" />
-   <article>
-    <p>
-      paragraph here
-    </p>
-  </article>
-  </div>
+
+  <p>Growing up, I never really knew what I wanted to do with my life. Yeah, I was one of those people who
+    stressed a lot about it. My first ambition was to become a ballerina (no joke), then a nurse, then a chef.
+    I basically had my mind set on becoming a chef. I do enjoy cooking.
+  </p>
+  <p>
+    Fast forward a few years later, we left the bustling city of Manila to travel halfway across the world
+    and move to the boot-shaped country, where I am right now. One day, I saw a graphic design course in town.
+    I signed up and was admitted, even if I barely knew Italian that time. And for the next two years, graphic design
+    basically took over my life. It still does; its something that would stick with me forever.</p>
+  <p>
+      Programming came shortly after that. I enjoyed every bit of time I spent learning how to code.
+      I've come a long way by teaching myself. It's a wild ride and its awesome.
+  </p>
+  <p>
+    I like being busy. When I'm not on the computer, you can probably find me breaking the routine, trying something new.
+  </p>
 </div>
+
+<div class="section">
+  <p>What's next?</p>
+</div>
+
+      <button id="note_link" href="#">Download my resume</button>    
+      <button id="note_link" href="contact.php">Leave a note</button>    
+
 
 <?= (file_get_contents("./templates/footer.php") )?>
