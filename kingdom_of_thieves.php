be75baf4e69c7f98539a0b7e1ec6612ff8c26927
Charisse <charisse.ysabel930@gmail.com>
2016-01-11 15:06:33 +0100

    Add Kingdom of Thieves page

---
+++ kingdom_of_thieves.php
@@ -0,0 +1,36 @@
+<?= (file_get_contents("./templates/header.php"))?>
+
+<div class="jumbotron">
+  <div class="container content header">
+    <h1>The Kingdom of Thieves</h1>
+    <p>Book Design &amp; Illustration</p>
+  </div>
+</div>
+
+<div class="container content article">
+  <h2>"Even the bad guys have a story to tell."</h2>
+    <p>
+      The Kingdom of Thieves was born after participating in the National Novel Writing Month, more commonly known as NaNoWrimo.
+      All the winners had the chance to self-publish their novels for free if they beat the fifty thousand word mark. A dream for every writer!<br>
+      Using the skills I earned as a graphic designer, I designed my book and drew illustrations inspired by Tolkien and other fantasy books that I loved. Being able to hold a book that was formed from my imagination gives an unmatchable feeling of accomplishment.<br>
+      The story, however, is not finished yet.
+    </p>    
+
+      <img src="../img/kingdom_of_thieves.jpg" class="work_img" alt="Kingdom of Thieves cover"/>
+
+      <p>Illustrating the cover of the book was a fun and challenging experience. By adding a mysterious character to the intruiging title, the book can attract attention to readers. Even my pen name, J. Drowseth, has a little backstory; it was the name of my very first novel character years before the Kingdom of Thieves was written. I tend to jumble plain English words when making up a name. Drowseth came from an adjective, "the sword".
+      </p>
+
+      <img src="../img/map.jpg" class="work_img" alt="Kingdom of Thieves map"/>
+
+      <p>
+        The map was an actual drawing that was scanned and cleaned up before inserting in the book. Adding it gives more life and keeps the readers from getting lost in a fantasy world. 
+      </p>
+
+      <img src="../img/kot_chapter.jpg" class="work_img" alt="Kingdom of Thieves book chapter"/>
+
+      <p>Writing fifty thousand words in a month eliminates the tempting task of going back and forth to edit and perfect the grammar, but its always nice to open the book and begin the journey again.</p>
+</div>
+
+
+<?= (file_get_contents("./templates/footer.php") )?>
