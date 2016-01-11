a470666e7790cb0450641275ec56e71cfbe6548b
Charisse <charisse.ysabel930@gmail.com>
2016-01-11 15:10:09 +0100

    Add link class to button

--- about.php
+++ about.php
@@ -1,7 +1,7 @@
 <?= (file_get_contents("./templates/header.php"))?>
 
 <div class="jumbotron">
-  <div class="container content header">
+  <div class="container content header plaintext">
     <h1>About</h1>
     <p>Hi, I'm Charisse.</p>
     <p>I'm a junior developer with a graphic design background.<br>
@@ -11,15 +11,15 @@
 
 <div class="container content article">
   
-  <img src="../img/charisse.jpg" id="me"/>
+  <img src="../img/charisse.jpg" alt="Me taking a picture of me" class="workImg"/>
 
   <div class="section">
       <p>A really brief history</p>
   </div>
 
-  <p>Growing up, I never really knew what I wanted to do with my life. Yeah, I was one of those people who
-    stressed a lot about it. My first ambition was to become a ballerina (no joke), then a nurse, then a chef.
-    I basically had my mind set on becoming a chef. I do enjoy cooking.<br>
+  <p>Growing up, I never really knew what I wanted to do with my life.
+    My first ambition was to become a ballerina (no joke), then a nurse, then a chef.
+    I basically had my mind set on becoming a chef. I do enjoy cooking, afterall.<br>
     Fast forward a few years later, we left the bustling city of Manila to travel halfway across the world
     and move to the boot-shaped country, where I am right now. One day, I saw a graphic design course in town.
     I signed up and was admitted, even if I barely knew Italian that time. And for the next two years, graphic design
@@ -29,16 +29,14 @@
     I like being busy. When I'm not on the computer, you can probably find me breaking the routine, trying something new.
   </p>
 
-<div class="section">
-  <p>What's next?</p>
-</div>
+  <div class="section">
+    <p>What's next?</p>
+  </div>
 
-<div class="link">
-<!--  <a class="cta" href="#">Download my resume</a>    -->
-      <a class="cta" href="contact.php">Leave a note</a>  
-</div>
-
-</div>  
+  <div class="link">
+  <!--  <a class="cta" href="#">Download my resume</a>    -->
+        <a class="cta" href="contact.php">Leave a note</a>  
+  </div>
 
 
 <?= (file_get_contents("./templates/footer.php") )?>
