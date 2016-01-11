153888bb0b59e104658677724d30bf998c361470
Charisse <charisse.ysabel930@gmail.com>
2016-01-11 15:12:45 +0100

    Remove double footer

--- rocket.php
+++ rocket.php
@@ -11,35 +11,29 @@
   <h2>"Designed to make accounting easy and fun."</h2>
     <p>
       Rocket is a web application submitted as a final project for a popular Massive Open Online Course (MOOC) by Harvard University: CS50x through Edx.org.<br>
-      The idea of making such an application came from seeing an Excel file that contained data of my family's coffee shop business.<br>
-      Spreadsheets are very useful but I wanted to make something that is much simpler but does not take away the basic accounting that small business owners are required to do.<br>
-      Built using:<br>HTML &#8226; CSS &#8226; JS &#8226; PHP &#8226; SQL
+      The idea of making such an application came from seeing an Excel file that contained data of my family's coffee shop business.
+      Spreadsheets are very useful but I wanted to make something that is much simpler but does not take away the basic accounting that small business owners are generally required to do.
     </p>
     
     <img src="../img/devices-rocket.png" class="work_img"/>
 
-        <p>Rocket is a web-based application. It works anywhere there is an
-          Internet connection so that you can study about the core of your business on the go, at anytime.
-        </p> 
-      
+    <p>
+      Making the application accessible via the web was one of the advantages of Rocket from spreadsheets that require a few extra steps
+      to be shared from one device to another. Using the techniques taught in the course, setting up the database and php controllers was a breeze. 
+    </p>
+
     <img src="../img/report.png" class="work_img"/>
 
-      <p>
-        Tracking the health of your business is a crucial task.
-        To save business owners from manually calculating their incomes and expenses, Rocket does it all for you
-        and displays the result using a lively set of graphs.<br>     
-        For every transaction recorded, deleted or edited, the graphs and summaries are updated as well.
-        Rocket handles the synchronization of your data so you do not have to do it yourself.<br>
-        To stay afloat when running a business, its always important to know where your money comes from and where you spend it.<br>      
-        The data entered in these two separate tables are the same data that are automatically generated in the overall report. Keeping the data correct is a must! 
-      </p>
+    <p>
+      But what fun is a final project if it was easy? When I decided to build a cashflow application, I initally had no idea how to
+      implement it -- I only had a problem and an idea on how to solve it. The biggest challenge I faced was using tools I have never used before, but which were a valuable part of the application that I cannot remove, or else I won't be crossing out the main objective of making things easier. A lot of things did not go as I intended to at first, but taking a step back, asking questions, reading documentations and searching in forums soon allowed me to fix the issues and make everything work.
+    </p> 
 
       <img src="../img/cashflow.png" class="work_img"/>
+
+    <p>
+      This first version of Rocket is still far from 'perfect'. The application still needs a lot of improvement but I am really proud of my work and how it turned out. I love the idea of it becoming a free open source application one day!
+    </p>
 </div>
-
-      <footer class="work_footer">
-        <p>&copy; 2015 Charisse Ysabel De Torres</p>
-    </footer>
-
 
 <?= (file_get_contents("./templates/footer.php") )?>
