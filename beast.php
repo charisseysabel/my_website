00f25da03d42a5728a7e3fad34703c05b46bb957
Charisse <charisse.ysabel930@gmail.com>
2016-01-17 15:56:37 +0100

    Add Beast and Urbino pages

---
+++ beast.php
@@ -0,0 +1,37 @@
+<?= (file_get_contents("./templates/header.php"))?>
+
+<div class="jumbotron">
+  <div class="container content header">
+    <h1>Beast</h1>
+    <p>Brochure design</p>
+  </div>
+</div>
+
+<div class="container content article">
+  <h2>"Unleash your inner Beast."</h2>
+    <p>
+      A brochure to promote a supercar by Rezvani Automotive Designs.
+      I was lucky that they gave me the freedom to design the brochure in any way that I think is best which gave me the opportunity get down and be creative. With it being my first freelance work as a junior designer, I made sure that the client was satisfied with my work.
+    </p>
+    
+    <img src="../img/beast/Cover-Beast.jpg" class="work_img" alt="Beast brochure cover"/>
+    <p>
+      The overall design of the brochure was based on the supercar's sleek and sharp features.
+    </p>
+
+    <p>
+    <img src="../img/beast/Spread1-Beast.jpg" class="work_img" alt="Beast brochure cover"/>
+    </p>
+
+    <p>
+    <img src="../img/beast/Spread2-Beast.jpg" class="work_img" alt="Beast brochure cover"/>
+    </p>
+
+    <p>
+    <img src="../img/beast/Spread3-Beast.jpg" class="work_img" alt="Beast brochure cover"/>
+    </p>
+
+    <img src="../img/beast/Back-Beast.jpg" class="work_img" alt="Beast brochure cover"/>
+</div>
+
+<?= (file_get_contents("./templates/footer.php") )?>
---
+++ urbino.php
@@ -0,0 +1,21 @@
+<?= (file_get_contents("./templates/header.php"))?>
+
+
+<div class="jumbotron">
+  <div class="container content header">
+    <h1>Urbino</h1>
+    <p>Poster design</p>
+  </div>
+</div>
+
+<div class="container content article">
+  <h2>"Guarda lontano! (Look beyond)"</h2>
+    <p>
+      A promotional material for the University of Urbino in Italy in collaboration with
+      studio Kaleidon and artist Tullio Pericoli.
+    </p>
+    
+    <img src="../img/urbino/urbino_poster.png" class="work_img" alt="urbino poster"/>
+</div>
+
+<?= (file_get_contents("./templates/footer.php") )?>
