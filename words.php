5391a5f70285f123c34e13a355627b1417507edb
Charisse <charisse.ysabel930@gmail.com>
2016-01-25 01:18:38 +0100

    Add Words project page

---
+++ words.php
@@ -0,0 +1,18 @@
+<?= (file_get_contents("./templates/header.php"))?>
+
+<div class="jumbotron">
+  <div class="container content header">
+    <h1>Words</h1>
+    <p>Front end development</p>
+  </div>
+</div>
+
+<div class="container content article">
+  <h2>""</h2>
+    <p>
+    </p>
+    
+
+</div>
+
+<?= (file_get_contents("./templates/footer.php") )?>
