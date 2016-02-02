811a95523bfa4992bba3a06f07669c0f1a437980
Charisse <charisse.ysabel930@gmail.com>
2016-02-02 18:35:26 +0100

    Add ideas page

---
+++ ideas.php
@@ -0,0 +1,18 @@
+<?= (file_get_contents("./templates/header.php"))?>
+<div class="fluid header-container">
+    <h1>Ideas</h1>
+    <p>Future projects</p>
+</div>
+
+<section class="chapter">
+      <p>Simplify</p>
+  </section>
+
+  <section class="text">
+    The idea for this application came from my personal desire to live a zero waste life.
+    But just like every other life-changing decision, to start is hard. So I'd like to build this application
+    to simplify the transition, our lives and ultimately, to help the environment. I want to make this app
+    for myself and for those willing to make a change. 
+  </section>
+
+<?= (file_get_contents("./templates/footer.php") )?>
