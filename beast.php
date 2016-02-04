ef969927305ccc744b8507ed3dd770f932a9dccc
Charisse <charisse.ysabel930@gmail.com>
2016-02-04 02:21:41 +0100

    Fix grid

--- beast.php
+++ beast.php
@@ -6,34 +6,23 @@
             
             <div class="fluid content-container">
                 <section class="text">
-                  A brochure to promote a supercar by Rezvani Automotive Designs.
-                  I was lucky that they gave me the freedom to design the brochure in any way that I think is best which gave me the opportunity get down and be creative. With it being my first freelance work as a junior designer, I made sure that the client was satisfied with my work.
+                    <p>
+                      A brochure to promote a supercar by Rezvani Automotive Designs.
+                      I was given the freedom to design it in any way that reflects the car which
+                      gave me the opportunity to be creative. The overall design of the brochure was based on the supercar's sleek and sharp features.
+                    </p>
                 </section>
                 
                 <div class="screenshot">
                     <img src="../img/beast/Cover-Beast.jpg" class="screenshot" alt="Beast brochure cover"/>
                 </div>
 
-                <section class="text">
-                  The overall design of the brochure was based on the supercar's sleek and sharp features.
-                </section>
-
                 <div class="grid-container">
-                    <div class="screenshot grid">
                         <img src="../img/beast/Spread1-Beast.jpg" alt="Beast brochure page"/>
-                    </div>
-
-                    <div class="screenshot grid">
                         <img src="../img/beast/Spread2-Beast.jpg" alt="Beast brochure page"/>
-                    </div>
 
-                    <div class="screenshot grid">
                         <img src="../img/beast/Spread3-Beast.jpg" alt="Beast brochure page"/>
-                    </div>
-
-                    <div class="screenshot grid">
                         <img src="../img/beast/Back-Beast.jpg" alt="Beast brochure back"/>
-                    </div>
                 </div><!-- /grid.container -->
             </div><!-- /content-container-->
 <?= (file_get_contents("./templates/footer.php") )?>
