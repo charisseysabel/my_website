1d5fb6d48dcc09e84fdbd78ef74cb57d7cab5d5d
Charisse <charisse.ysabel930@gmail.com>
2016-02-08 16:35:49 +0100

    Modify text

--- words.php
+++ words.php
@@ -6,12 +6,24 @@
 
 <div class="fluid content-container">
     <section class="text">
-    	<p>
-    		Words is an ongoing personal project that aims to explain the meaning of rare English words
-    		through web graphics and animation. By trying different techniques, I can improve my front end
-    		skills and learn new words as well!
-    	</p>
+        <p>
+            Words is an ongoing personal project that aims to explain the meaning of rare English words
+            through web graphics and animation. By trying different techniques, I can improve my front end
+            skills and learn new words as well.<br>
+            I spend a lot of time conducting online research for words, handpicking the ones that are truly useful,
+            or something that we can relate to in our every day life before finally coming up with a simple animation
+            to make it easier for people to understand. These factors make the progress quite slow, but its fun, different and
+            extremely rewarding.
+        </p>
     </section>
+
+    <div class="content-links">
+    <p class="link">
+      <a href="http://charisseysabel.github.io/words/">
+        Learn a word
+      </a>
+    </p>
+  </div><!-- /content-links -->
 </div>
 
 <?= (file_get_contents("./templates/footer.php") )?>
