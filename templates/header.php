f5780ccc889d029ae243575e179c1cdf395fb32f
Charisse <charisse.ysabel930@gmail.com>
2016-02-16 23:22:25 +0100

    Add meta tags

--- templates/header.php
+++ templates/header.php
@@ -4,6 +4,11 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Charisse Ysabel De Torres is a junior web developer and graphic designer.
+        This is her place on the web.">
+    <meta name="keywords" content="Web Developer, Front End Developer, Web designer, Graphic designer, Junior developer,
+    Web developer portfolio, Design portfolio">
+    <meta name="author" content="Charisse Ysabel De Torres"> 
 
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300' rel='stylesheet' type='text/css'>
 
